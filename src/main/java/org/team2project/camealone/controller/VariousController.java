7431ddf728697710f5f9e8c03a52522acc5c4c9a
Hyesung0426 <fkfkqkfk@naver.com>
2024-06-06 21:56:17 +0900

    w

--- src/main/java/org/team2project/camealone/controller/VariousController.java
+++ src/main/java/org/team2project/camealone/controller/VariousController.java
@@ -1,35 +1,22 @@
 package org.team2project.camealone.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VariousController {
+
     @GetMapping("/date") //  날짜 선택 페이지
-    public String getDate(){
-        return "Date_select";
-    }
+    public String getDate(){return "Date_select";}
+
     @GetMapping("/Restaurant_Weather")
     public String Restaurant() {
         return "Restaurant_Weather";
     }
 
-    @GetMapping("/areaDropdown")
-    public String AreaDropdown() {
-        return "AreaDropdown";
-    }
-
     @GetMapping("/MyPage")
-    public String MyPage(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute("user") == null) {
-            return "redirect:/user/login";
-        }else {
-            return "MyPage";
-        }
-    }
+    public String MyPage() {return "MyPage";}
+
     @GetMapping("/list")
     public String List() {
         return "list";
